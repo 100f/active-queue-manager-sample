1e7dd594d8581d78162809f0362dce622bf11de2
Caio Enrique <cemf1996@gmail.com>
2022-06-18 19:55:31 -0300

    feat: implementação do consumidor

---
+++ consumer/src/main/java/dev/caio/study/queue/consumer/config/RabbitMQConfig.java
@@ -0,0 +1,44 @@
+package dev.caio.study.queue.consumer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${spring.rabbitmq.queue-name}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.exchange-name}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.message-name}")
+    private String routingKey;
+
+    @Bean
+    public Queue getQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Exchange getExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding buildQueueExchangeBinding(Queue queue, Exchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingKey)
+                .noargs();
+    }
+
+    @Bean
+    public MessageConverter getMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
