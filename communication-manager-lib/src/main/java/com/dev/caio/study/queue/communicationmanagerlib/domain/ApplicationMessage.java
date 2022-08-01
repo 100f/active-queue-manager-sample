2c52aae121e194e04040cc58a76a24be09217535
Caio Enrique <cemf1996@gmail.com>
2022-08-01 00:59:29 -0300

    refactor: alterando composição de cabeçalho e payload da mensagem para que seja gerenciável em consumidores

--- communication-manager-lib/src/main/java/com/dev/caio/study/queue/communicationmanagerlib/domain/ApplicationMessage.java
+++ communication-manager-lib/src/main/java/com/dev/caio/study/queue/communicationmanagerlib/domain/ApplicationMessage.java
@@ -3,33 +3,20 @@ package com.dev.caio.study.queue.communicationmanagerlib.domain;
 import lombok.Getter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-public class ApplicationMessage implements Message<Map<String, Serializable>> {
-    private Map<String, Serializable> payload;
-    private Map<String, Object> headers;
+public class ApplicationMessage extends GenericMessage<Map<String, Serializable>> {
 
     public ApplicationMessage(String messageType, String ownerApp, String rotulo, Map<String, Serializable> payload) {
-        this.payload = payload;
-        this.headers = buildHeaders(messageType, ownerApp, rotulo);
+        super(payload, buildHeaders(messageType, ownerApp, rotulo));
     }
 
-    @Override
-    public Map<String, Serializable> getPayload() {
-        return payload;
-    }
-
-
-    @Override
-    public MessageHeaders getHeaders() {
-       return new MessageHeaders(this.headers);
-    }
-
-    private Map<String, Object> buildHeaders(String messageType, String ownerApp, String rotulo) {
+    private static Map<String, Object> buildHeaders(String messageType, String ownerApp, String rotulo) {
         Map<String, Object> builtHeaders = new HashMap<>();
         builtHeaders.put("MESSAGE_TYPE", messageType);
         builtHeaders.put("ROTULO", rotulo);
